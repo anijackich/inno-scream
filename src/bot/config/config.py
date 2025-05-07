3a07bd71f68aac2410f14e919a3f06bf70aa0e42
anijackich <anijackich@gmail.com>
2025-05-08 02:00:05 +0300

    chore(dotenv support): add dotenv support

--- src/bot/config/config.py
+++ src/bot/config/config.py
@@ -1,17 +1,28 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+DOTENV_FILES = ('.env', './.envs/bot/.env')
+
+dotenv_settings_config = SettingsConfigDict(
+    extra='ignore',
+    env_file=DOTENV_FILES,
+    env_file_encoding='utf-8',
+)
+
 
 class TelegramBot(BaseSettings):
     token: str = Field(..., validation_alias='telegram_bot_token')
     admins: list[int] = Field(...)
     reactions: list[str] = Field(...)
 
+    model_config = dotenv_settings_config
+
 
 class InnoScream(BaseSettings):
     base_url: str = Field(...)
 
-    model_config = SettingsConfigDict(env_prefix='innoscream_')
+    model_config = dotenv_settings_config
+    model_config['env_prefix'] = 'innoscream_'
 
 
 class Settings(BaseSettings):
