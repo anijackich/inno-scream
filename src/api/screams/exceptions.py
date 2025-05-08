f7d72ce8ce9bcbac3b3e6f8a250b0eb1784e3997
2IMT <daniil_t04@mail.ru>
2025-05-08 19:01:48 +0300

    chore(docstrings for api): add docstrings for api/screams/exceptions

--- src/api/screams/exceptions.py
+++ src/api/screams/exceptions.py
@@ -1,4 +1,15 @@
+"""`screams` exceptions."""
+
+
 class ScreamNotFound(Exception):
+    """Scream not found."""
+
     def __init__(self, message: str = 'Scream not found'):
+        """
+        Create ScreamNotFound instance.
+
+        Args:
+            message (str): Exception message
+        """
         self.message = message
         super().__init__(message)
