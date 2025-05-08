e54e0f20e0ed2e80100a3fa48eae7a31a5373187
2IMT <daniil_t04@mail.ru>
2025-05-08 20:11:26 +0300

    chore(docstrings for api): add docstrings for api/errors

--- src/api/errors.py
+++ src/api/errors.py
@@ -1,3 +1,5 @@
+"""API errors."""
+
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
@@ -5,6 +7,7 @@ from api.screams import ScreamNotFound
 
 
 async def scream_not_found_handler(request: Request, exc: ScreamNotFound):
+    """Handle ScreamNotFoundError."""
     return JSONResponse(
         status_code=404,
         content={'message': exc.message},
@@ -12,4 +15,5 @@ async def scream_not_found_handler(request: Request, exc: ScreamNotFound):
 
 
 def register_exception_handler(app):
+    """Register exception handler for application."""
     app.add_exception_handler(ScreamNotFound, scream_not_found_handler)
